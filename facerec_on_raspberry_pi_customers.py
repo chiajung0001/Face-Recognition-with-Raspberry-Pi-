34cbb0cf9642a2864e888d3dad3ce40fd10c4c00
chiajung0001 <48582706+chiajung0001@users.noreply.github.com>
2019-07-04 22:22:26 -0700

    Add files via upload

---
+++ facerec_on_raspberry_pi_customers.py
@@ -0,0 +1,63 @@
+import face_recognition
+import picamera
+import numpy as np
+import os
+import time
+
+#初始化
+camera = picamera.PiCamera()
+#設置相機解析度
+camera.resolution = (320, 240)
+
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+
+print("Loading known face image(s)")
+
+
+face_locations = []
+face_encodings = []
+face_names = []
+
+known_person=[]
+known_image=[]
+known_face_encoding=[]
+
+
+# 創建一個名為"customers"資料夾並從裡面的會員人臉抓取人臉特徵並以檔案名稱為人臉名稱
+for file in os.listdir("customers"):
+    try:
+        known_person.append(file.replace(".jpg", ""))
+        file=os.path.join("customers/", file)
+        known_image = face_recognition.load_image_file(file)
+        known_face_encoding.append(face_recognition.face_encodings(known_image)[0])
+    except Exception as e:
+        pass
+
+
+
+while True:
+    print("Capturing image.")
+    #從相機抓取一幀圖像轉為numpy陣列
+    camera.capture(output, format="rgb")
+
+    # 找出當前視頻中所有的人臉及臉部編碼
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+
+    # 循環檢查框架中的人臉是否為已知的會員人臉
+    for face_encoding in face_encodings:
+
+        match = face_recognition.compare_faces(known_face_encoding, face_encoding, tolerance=0.45)
+        matches=np.where(match)[0] #檢查哪個圖片是符合的
+        if len(matches)>0:
+          name = str(known_person[matches[0]])
+        else:
+          name = "Unknown"
+
+        print("I see someone named {}!".format(name), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+
+
+
